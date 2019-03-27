8bb728f35d116add10fc3063e7e2f7fd412ee034
w28ahmad <43382198+w28ahmad@users.noreply.github.com>
2019-03-26 22:54:26 -0400

    Removing comments

---
+++ Notes App/notes.js
@@ -0,0 +1,68 @@
+const yargs = require('yargs')
+
+const func = require("../notesFunctions.js")
+
+yargs.version('1.1.0')
+yargs.command({
+ command: 'add',
+ describe: 'Add a new note',
+ handler: function () {
+ console.log('Adding a new note!')
+ }
+})
+
+yargs.command({
+    command: "add",
+    describe: "Add a new note",
+    builder: {
+        title:{
+            describe: "Note Tite",
+            demandOption: true,
+            type: 'string'
+        },
+        body:{
+            describe: "Note Body",
+            demandOption:true,
+            type: 'string'
+        }
+        
+    },
+    handler: function(argv){
+        console.log("Adding note");
+        add(argv);
+    }
+}).command({
+    command: "remove",
+    describe: "Remove a note",
+    builder: {
+        title: {
+            describe: "Note Title",
+            demand: true,
+            type: "string"
+        }
+    },
+    handler: function(argv){
+        console.log("Removing the note...")
+        remove(argv.title);
+    }
+}).command({    
+    command: "list",
+    describe: "list all notes",
+    handler: function(){
+        list();
+    }
+}).command({
+    command: "read",
+    describe: "Read a note",
+    builder:{
+        title:{
+            describe: "Note title",
+            demand: true,
+            type: "String"
+        }
+    },
+    handler: function(argv){
+        console.log("Finding the note");
+        read(argv.title);
+    }
+}).argv
